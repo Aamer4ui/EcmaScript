d5b325542c390385f1362b9637454a30c568ef93
Aamer4ui <aamersohelansari@gmail.com>
2021-07-15 11:31:47 -0700

    changing value

--- BiggernumOpt.js
+++ BiggernumOpt.js
@@ -6,4 +6,4 @@ const BiggerNumOpt = (num1 , num2 , num3 , num4) =>{
     return BadaNum;
 };
 
-console.log(`Bigger Number Is ${BiggerNumOpt(23, 45 ,85 , 55)}`)
+console.log(`Bigger Number Is ${BiggerNumOpt(23, 115 ,85 , 55)}`)
