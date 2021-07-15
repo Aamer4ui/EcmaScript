f55ded87c5bb6f6b28441a64239d94af983183d3
Aamer4ui <aamersohelansari@gmail.com>
2021-07-15 11:54:21 -0700

    diffrence age in minus and big number optimum

--- BiggernumOpt.js
+++ BiggernumOpt.js
@@ -1,9 +1,10 @@
-const BiggerNumOpt = (num1 , num2 , num3 , num4) =>{
-    let BadaNum = 0;
-    BadaNum = num1 > num2 ? num1 : num2;
-    BadaNum = BadaNum > num3 ? BadaNum : num3;
-    BadaNum = BadaNum > num4 ? BadaNum : num4;
-    return BadaNum;
-};
+const BiggerNumOpt = (num1 , num2 , num3 , num4) =>
+    // let BadaNum = 0;
+    // BadaNum = num1 > num2 ? num1 : num2;
+    // BadaNum = BadaNum > num3 ? BadaNum : num3;
+    // BadaNum = BadaNum > num4 ? BadaNum : num4;
+    // return BadaNum;
+
+    Math.max (num1 , num2 , num3 , num4); /// other option optimum
 
 console.log(`Bigger Number Is ${BiggerNumOpt(23, 115 ,85 , 55)}`)
--- agediff.js
+++ agediff.js
@@ -1,3 +1,4 @@
-const ageDiff =(num1 , num2) => num1 - num2;
+const ageDiff =(num1 , num2) => Math.abs (num1 - num2);
 
 console.log(`Age Diffrence is ${(ageDiff(25,15))} Years`);
+console.log(`Age Diffrence is ${(ageDiff(25,45))} Years`);
