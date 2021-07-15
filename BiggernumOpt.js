9111b1c1fd8892dfa0fb9b79f0798f13f57b9b7e
Aamer4ui <aamersohelansari@gmail.com>
2021-07-15 11:31:08 -0700

    Bigger Num Optimum

--- BiggernumOpt.js
+++ BiggernumOpt.js
@@ -1,3 +1,9 @@
-const BiggerNumOpt = (num1 , num2 , num3 , num4) =>{};
+const BiggerNumOpt = (num1 , num2 , num3 , num4) =>{
+    let BadaNum = 0;
+    BadaNum = num1 > num2 ? num1 : num2;
+    BadaNum = BadaNum > num3 ? BadaNum : num3;
+    BadaNum = BadaNum > num4 ? BadaNum : num4;
+    return BadaNum;
+};
 
 console.log(`Bigger Number Is ${BiggerNumOpt(23, 45 ,85 , 55)}`)
