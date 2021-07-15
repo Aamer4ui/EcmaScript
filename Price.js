535f471e086cb3b5edd0844aa5ad201a1153da89
Aamer4ui <aamersohelansari@gmail.com>
2021-07-15 11:58:49 -0700

    price increase

--- Price.js
+++ Price.js
@@ -1,2 +1,4 @@
 const BikeMillage = (Dis , Mill , Pri ) => Math.round((Dis/Mill)*Pri);
-console.log(`Total Amount Is ${BikeMillage(404, 47 ,109.05)} `);
+console.log(`Total Amount Is ${BikeMillage(404, 47 ,109.05)}`);
+
+console.log(Math.ceil(44.37)); //// to increase full amount with full amount
