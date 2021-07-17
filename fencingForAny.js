3e669061d318826c93e342c7d5bafb3356d8e12f
Aamer4ui <aamersohelansari@gmail.com>
2021-07-16 23:18:53 -0700

    fencing to any shape

--- fencingForAny.js
+++ fencingForAny.js
@@ -1,9 +1,14 @@
 const FencingAny = (edges, numberWire, rate) => {
     let sum = 0;
-    for(let i = 0; i < edges.length; i++){
-        sum = sum + edges[i];
-    }
+    // for(let i = 0; i < edges.length; i++){
+    //     sum = sum + edges[i];
+    // }
+    // return sum * numberWire * rate;
+    edges.forEach(element => {
+        sum = sum+element;
+        
+    });
     return sum * numberWire * rate;
 
 };
-console.log(FencingAny([45,15,5,25],3,8));
+console.log(`Cost for Fencing is ${FencingAny([45,15,5,25],3,8)}/-`);
